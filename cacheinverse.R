54622962dd5f3aafc61f23db2e835f2941511878
NERVE-SHEN <54467236+NERVE-SHEN@users.noreply.github.com>
2019-08-24 22:07:15 +0800

    Update cacheinverse.R

--- cacheinverse.R
+++ cacheinverse.R
@@ -1,11 +1,12 @@
+##This function will compute the inverse of the matrix
 cacheinverse<-function(x,...){
 	inv<-x$getinverse()
-	if(!is.null(inv)){
+	if(!is.null(inv)){			##It will check to see if the inverse has already been calculated
 		message('getting cached data')
 		return(inv)
 	}
 	data<-x$get()
-	inv<-solve(data,...)
+	inv<-solve(data,...)			##I use solve() to get the inverse of the matrix
 	x$setinverse(inv)
-	inv
+	inv			##The output
 }
